2f7352b7f040cbdb936038fd8528d1a2b93f4d6e
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-15 19:08:54 +0330

    Added a method to add question with answers.

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,24 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        System.out.println("\n");
-        System.out.println("Choose an option: \n1. Ask a question\n2. Add a question");
-        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n");
+            System.out.println("Choose an option: \n1. Ask a question\n2. Add a question");
+            Scanner scanner = new Scanner(System.in);
 
-        String choice = scanner.nextLine().trim();
-        MainService mainService = new MainService();
-        if (choice.equals("1")) {
-            System.out.println("Enter your question:");
-            String question = scanner.nextLine().trim();
+            String choice = scanner.nextLine().trim();
+            MainService mainService = new MainService();
+            if (choice.equals("1")) {
+                System.out.println("Enter your question:");
+                String question = scanner.nextLine().trim();
 
-        } else if (choice.equals("2")) {
-            System.out.println("Enter your question and answers (e.g., What is Peters favorite food? \"Pizza\" \"Spaghetti\"):");
-            String input = scanner.nextLine().trim();
-            mainService.addQuestion(input);
-        } else {
-            System.out.println("Invalid option. Please select 1 or 2.");
+            } else if (choice.equals("2")) {
+                System.out.println("Enter your question and answers (e.g., What is Peters favorite food? \"Pizza\" \"Spaghetti\"):");
+                String questionAnswer = scanner.nextLine().trim();
+                mainService.addQuestion(questionAnswer);
+            } else {
+                System.out.println("Invalid option. Please select 1 or 2.");
+            }
         }
     }
 }
